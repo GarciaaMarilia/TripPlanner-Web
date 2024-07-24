cc3c1d7e8847be013a67872fae73bd34ef873eac
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2024-07-24 11:14:45 +0200

    important links api + fav icon

--- src/pages/create-trip/trip-details/important-links.tsx
+++ src/pages/create-trip/trip-details/important-links.tsx
@@ -1,41 +1,61 @@
+import { useEffect, useState } from "react";
+
 import { Link2, Plus } from "lucide-react";
 
+import { api } from "../../../lib/axios";
+import { useParams } from "react-router-dom";
 import { Button } from "../../../components/button";
 
+interface Link {
+ title: string;
+ url: string;
+}
+
 export function ImportantLinks() {
+ const { tripId } = useParams();
+ const [links, setLinks] = useState<Link[]>([]);
+
+ useEffect(() => {
+  api.get(`trips/${tripId}/links`).then((response) => setLinks(response.data));
+ }, [tripId]);
+
  return (
   <div className="space-y-6">
-   <h2 className="font-semibold text-xl">Important <link rel="alternate" href="atom.xml" type="application/atom+xml" title="Atom" />links</h2>
+   <h2 className="font-semibold text-xl">
+    Important{" "}
+    <link
+     rel="alternate"
+     href="atom.xml"
+     type="application/atom+xml"
+     title="Atom"
+    />
+    links
+   </h2>
+   {links.length > 0 ? (
+    <div className="space-y-5">
+     {links.map((link) => {
+      return (
+       <div className="flex items-center justify-between gap-4">
+        <div className="space-y-1.5">
+         <span className="block font-medium text-zinc-100">{link.title}</span>
+         <a
+          href="#"
+          className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+         >
+          {link.url}
+         </a>
+        </div>
 
-   <div className="space-y-5">
-    <div className="flex items-center justify-between gap-4">
-     <div className="space-y-1.5">
-      <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
-      <a
-       href="#"
-       className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
-      >
-       https://www.airbnb.com.br/rooms/10470001139028321098312093821903812038910
-      </a>
-     </div>
-
-     <Link2 className="text-zinc-400 size-5 shrink-0" />
+        <Link2 className="text-zinc-400 size-5 shrink-0" />
+       </div>
+      );
+     })}
     </div>
-
-    <div className="flex items-center justify-between gap-4">
-     <div className="space-y-1.5">
-      <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
-      <a
-       href="#"
-       className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
-      >
-       https://www.airbnb.com.br/rooms/10470001139028321098312093821903812038910
-      </a>
-     </div>
-
-     <Link2 className="text-zinc-400 size-5 shrink-0" />
-    </div>
-   </div>
+   ) : (
+    <p className="text-zinc-500 text-sm">
+     There are no important links for this trip.
+    </p>
+   )}
 
    <Button variant="secondary" size="full">
     <Plus className="size-5" />
