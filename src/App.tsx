7b1705e1665a9ec7a46e4b17278e5ae22e7ca826
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2024-08-14 11:01:09 +0200

    router fixed

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,31 @@ import { AuthProvider } from "./contexts/AuthContext";
 import { TripDetailsPage } from "./pages/create-trip/trip-details";
 import { ListTripsPage } from "./pages/create-trip/list-trips/list-trips";
 
-const router = createBrowserRouter([
- {
-  path: "/",
-  element: <LoginPage />,
- },
- {
-  path: "/createTrip",
-  element: <ProtectedRoute element={<CreateTripPage />} />,
- },
- {
-  path: "/trips/:tripId",
-  element: <ProtectedRoute element={<TripDetailsPage />} />,
- },
- {
-  path: "/listTrips/:userId",
-  element: <ProtectedRoute element={<ListTripsPage />} />,
- },
-]);
+const router = createBrowserRouter(
+ [
+  {
+   path: "/",
+   element: <LoginPage />,
+  },
+  {
+   path: "/createTrip",
+   element: <ProtectedRoute element={<CreateTripPage />} />,
+  },
+  {
+   path: "/trips/:tripId",
+   element: <ProtectedRoute element={<TripDetailsPage />} />,
+  },
+  {
+   path: "/listTrips/:userId",
+   element: <ProtectedRoute element={<ListTripsPage />} />,
+  },
+  {
+   path: "*",
+   element: <h1>404: Page Not Found</h1>,
+  },
+ ],
+ { basename: "/TripPlanner-Web" }
+);
 
 export function App() {
  return (
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/TripPlanner-Web/'
-})
+export default defineConfig(({ mode }) => {
+  const base = mode === 'production' ? '/TripPlanner-Web/' : '/';
+
+  return {
+    plugins: [react()],
+    base,
+  };
+});
