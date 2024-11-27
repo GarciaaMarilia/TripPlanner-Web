1fd3b79d8d738a3ec676512691a83c91125b8e2e
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2024-11-27 16:40:21 +0100

    api url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 export const api = axios.create({
- //  baseURL: "https://TripPlanner.railway.app",
- baseURL: "http://localhost:3333",
+ baseURL: API_URL,
 });
